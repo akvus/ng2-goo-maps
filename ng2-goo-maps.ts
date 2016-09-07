a12adabecb5a39e04d439c94c7ce1352c058c64b
Maciej Zawieja <mr.akvus@gmail.com>
2016-09-07 23:16:42 +0300

    Triple slash references removed.

--- ng2-goo-maps.ts
+++ ng2-goo-maps.ts
@@ -1,6 +1,3 @@
-///<reference path="node_modules/angular2/typings/browser.d.ts"/>
-///<reference path="typings/index.d.ts"/>
-
 export * from './src/goo-maps.module';
 export * from './src/interface/definitions';
 export * from './src/service/goo-maps-api.service';
